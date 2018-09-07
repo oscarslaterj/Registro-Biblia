3acec707af20647e2e3f54419f553fab40ea4b13
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-07 08:12:04 -0400

    Creando MainForm

--- Registro_Biblia/Program.cs
+++ Registro_Biblia/Program.cs
@@ -16,7 +16,7 @@ namespace Registro_Biblia
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
