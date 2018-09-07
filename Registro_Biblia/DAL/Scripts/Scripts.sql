31d25814497cb1462e8b8c7390baea483290052c
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-07 08:22:49 -0400

    Creando Script

---
+++ Registro_Biblia/DAL/Scripts/Scripts.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE LibrosDb
+GO
+USE LibrosDb
+GO
+CREATE TABLE Libros
+(
+	LibroId int primary key identity (1,1),
+	Descripcion varchar(30),
+	Siglas varchar(13),
+	TipoId varchar(13),
+);
